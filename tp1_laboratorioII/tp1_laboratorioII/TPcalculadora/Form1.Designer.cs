8128c4c72e62ea4dda2fda1584f9327382b52f1f
santiortizutn <santyno09@gmail.com>
2016-09-12 03:38:01 +0200

    Agregado del metodo faltante

---
+++ tp1_laboratorioII/tp1_laboratorioII/TPcalculadora/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TPcalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.cmbOperacion = new System.Windows.Forms.ComboBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.ForeColor = System.Drawing.Color.Gray;
+            this.lblResultado.Location = new System.Drawing.Point(26, 18);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "Resultado";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(29, 45);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(69, 23);
+            this.txtNumero1.TabIndex = 1;
+            this.txtNumero1.TextChanged += new System.EventHandler(this.txtNumero1_TextChanged);
+            // 
+            // cmbOperacion
+            // 
+            this.cmbOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOperacion.FormattingEnabled = true;
+            this.cmbOperacion.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbOperacion.Location = new System.Drawing.Point(113, 45);
+            this.cmbOperacion.Name = "cmbOperacion";
+            this.cmbOperacion.Size = new System.Drawing.Size(50, 24);
+            this.cmbOperacion.TabIndex = 2;
+            this.cmbOperacion.SelectedIndexChanged += new System.EventHandler(this.cmbOperacion_SelectedIndexChanged);
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(179, 45);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(69, 23);
+            this.txtNumero2.TabIndex = 3;
+            this.txtNumero2.TextChanged += new System.EventHandler(this.txtNumero2_TextChanged);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(29, 132);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(93, 30);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "CC";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(143, 132);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(93, 30);
+            this.btnOperar.TabIndex = 5;
+            this.btnOperar.Text = "=";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 184);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.cmbOperacion);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.ComboBox cmbOperacion;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnOperar;
+    }
+}
+
---
+++ tp1_laboratorioII/tp1_laboratorioII/TPcalculadora/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPcalculadora
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbOperacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.limpiar();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultadoAux;
+            Numero numero1;
+            Numero numero2;
+            string aux;
+            
+            numero1 = new Numero(this.txtNumero1.Text);
+            numero2 = new Numero(this.txtNumero2.Text);
+
+            resultadoAux = Calculadora.operar(numero1, numero2, this.cmbOperacion.Text);
+            
+            aux = Convert.ToString(resultadoAux);
+
+            this.lblResultado.Text = aux;
+
+
+        }
+
+        private void limpiar()
+        {
+            this.lblResultado.Text = "Resultado";
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.cmbOperacion.Text = string.Empty;
+        }
+    }
+}
