7b02372f6eccfd6ddb8214b3d28f91555e0c8867
Santiago ortiz <santyno09@gmail.com>
2016-11-18 15:13:19 -0300

    listo

---
+++ TrabajoPracticoNº3/Archivos_/Texto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excepciones;
+
+
+namespace Archivos
+{
+    public class Texto:IArchivo<string>
+    {
+
+        public bool guardar(string archivo, string datos)
+        {
+            try
+            {
+                StreamWriter escritor = new StreamWriter(archivo, false);
+
+                escritor.Write(datos);
+
+                escritor.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
+        }
+
+        public bool leer(string archivo, out string datos)
+        {
+            try
+            {
+                StreamReader lector = new StreamReader(archivo);
+
+                datos = lector.ReadToEnd();
+
+                lector.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                datos = default(string);
+                throw new ArchivosException(e);
+            }
+        }
+    }
+}
---
+++ TrabajoPracticoNº3/Excepciones_/NacionalidadInvalidaException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class NacionalidadInvalidaException : Exception
+    {
+        public NacionalidadInvalidaException()
+            : base("La nacionalidad no es valida.")
+        { }
+
+
+        public NacionalidadInvalidaException(string message):base(message)
+        { }
+    }
+}
