cbcddbf476ccf23ef3c1061459a5b8258987805e
Santiago ortiz <santyno09@gmail.com>
2016-11-26 13:12:15 -0300

    tp4 listo

---
+++ RECUPERATORIO/TP4/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public string rutaArchivo;
+
+        /// <summary>
+        /// Constructor que asigna la ruta del archivo.
+        /// </summary>
+        /// <param name="archivo">Ruta del archivo.</param>
+        public Texto(string archivo)
+        {
+            this.rutaArchivo = archivo;
+        }
+
+        /// <summary>
+        /// Guarda los datos.
+        /// </summary>
+        /// <param name="datos">Datos a guardar.</param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter escritor = new StreamWriter(this.rutaArchivo, true);
+                escritor.WriteLine(datos);
+                escritor.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                
+                throw new Exception("NO GUARDA.");
+            }
+        }
+
+        /// <summary>
+        /// Lee los datos.
+        /// </summary>
+        /// <param name="datos">Datos a leer.</param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            try
+            {
+                StreamReader lector;
+                lector = new StreamReader(this.rutaArchivo);
+
+                datos = new List<string>();
+
+                while (lector.EndOfStream == false)
+                {
+                    datos.Add(lector.ReadLine());
+                }
+
+                lector.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                datos = default(List<string>);
+                throw new Exception("NO LEE.");
+            }
+
+        }
+    }
+}
---
+++ RECUPERATORIO/TP4/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public delegate void EventoDescargaFinalizada(string html);
+    public delegate void EventoPorcentajeDeDescarga(int porcentaje);
+
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        /// <summary>
+        /// Constructor que asigna la direccion.
+        /// </summary>
+        /// <param name="direccion"></param>
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        /// <summary>
+        /// Se descarga la web pasada anteriormente.
+        /// </summary>
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+
+                cliente.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public event EventoPorcentajeDeDescarga PorcentajeDescarga;
+        /// <summary>
+        /// Metodo que lanza un evento que actualiza el porcentaje de la descarga.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.PorcentajeDescarga(e.ProgressPercentage);
+        }
+
+
+        public event EventoDescargaFinalizada DescargaTerminada;
+        /// <summary>
+        /// Metodo que lanza un evento que posee el codigo descargado. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            this.DescargaTerminada(this.html);
+        }
+    }
+}
